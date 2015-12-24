fea4f9a84bf25ed5b2a85a02791b5e8b28e5ed3f
Emanuele Firmani <emanuele.firmani@outlook.com>
2015-12-24 17:17:07 +0100

    Service uses Owin for startup. TestController exposes Get operation

---
+++ LHAL.WebAPI.Test/TestControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace LHAL.WebAPI.Test
+{
+    class TestControllerTest
+    {
+        [NUnit.Framework.Test]
+        public void GetReturnsOK()
+        {
+            var address = "http://localhost:9000/";
+
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>(url: address))
+            {
+                var request = new RestRequest("api/test", Method.GET);
+
+                var client = new RestClient(address);
+                var response = client.Execute(request);
+
+                response.ResponseStatus.Should().Be(ResponseStatus.Completed);
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+                var der = new RestSharp.Deserializers.JsonDeserializer();
+                der.Deserialize<string>(response).Should().Be("OK");
+            }
+        }
+    }
+}
--- LHAL.WebAPI/Startup.cs
+++ LHAL.WebAPI/Startup.cs
@@ -1,24 +1,27 @@
-﻿using System;
+﻿using Microsoft.Owin;
+using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Http;
 
 namespace LHAL.WebAPI
 {
-    public static class WebApiConfig
+    [assembly: OwinStartup(typeof(Startup))]
+    public class Startup
     {
-        public static void Register(HttpConfiguration config)
+        public void Configuration(IAppBuilder app)
         {
-            // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+            var config = new HttpConfiguration();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            app.UseWebApi(config);
         }
     }
 }
