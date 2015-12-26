7d09056d2b5748b2b51242b9402b12ce150c9472
Emanuele Firmani <emanuele.firmani@outlook.com>
2015-12-26 22:51:15 +0100

    LocalDB is created during tests and filled up with Dapper

--- LHAL.WebAPI.Test/Integration/Fixtures.cs
+++ LHAL.WebAPI.Test/Integration/Fixtures.cs
@@ -2,10 +2,13 @@
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
+using Dapper;
 
 namespace LHAL.WebAPI.Test.Integration
 {
@@ -20,6 +23,35 @@ namespace LHAL.WebAPI.Test.Integration
         {
             Microsoft.Owin.Hosting.WebApp.Start<Startup>(url: ADDRESS);
             Client = new RestClient(ADDRESS);
+            InitDB();
+        }
+
+        [OneTimeTearDownAttribute]
+        public void TearDown()
+        {
+            Database.Delete((new DAL.LHAL_AppEntities()).Database.Connection);
+        }
+
+        private void InitDB()
+        {
+            Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseAlways<DAL.LHAL_AppEntities>());
+            // Forces initialization of database on model changes. Needed in order to avoid "Login failed" error when accessing DB
+            using (var context = new DAL.LHAL_AppEntities())
+            {
+                context.Database.Initialize(force: true);
+            }  
+
+            using (var conn = new SqlConnection((new DAL.LHAL_AppEntities()).Database.Connection.ConnectionString))
+            {
+                conn.Execute("INSERT INTO [dbo].[Giocatore]([Nome], [Cognome], [ExTesserato]) VALUES(@name, @lastname, 0)",
+                    new [] {
+                        new { lastname = "Black", name = "John" },
+                        new { lastname = "Black", name = "Tim" },
+                        new { lastname = "Bear", name = "Steve" },
+                        new { lastname = "White", name = "Tim" }
+                    }
+                );
+            }
         }
     }
 
