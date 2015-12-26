7d1ef04f6dd2ad96e00171fa197bfd278b394d9c
Emanuele Firmani <emanuele.firmani@outlook.com>
2015-12-26 01:27:17 +0100

    Tests use SetUpFixture for creating WebApp

---
+++ LHAL.WebAPI.Test/Integration/Fixtures.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHAL.WebAPI.Test.Integration
+{
+    [SetUpFixture]
+    public class Fixtures
+    {
+        internal const string ADDRESS = "http://localhost:9000/";
+
+        [OneTimeSetUpAttribute]
+        public void SetUp()
+        {
+            Microsoft.Owin.Hosting.WebApp.Start<Startup>(url: ADDRESS);
+        }
+    }
+}
---
+++ LHAL.WebAPI.Test/Integration/TestControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using RestSharp;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace LHAL.WebAPI.Test.Integration
+{
+    class TestControllerTest
+    {
+        [NUnit.Framework.Test]
+        public void EchoReturnsEchoedStringWithGET()
+        {
+            var request = new RestRequest("api/test", Method.GET);
+            request.AddParameter("message", "Tim");
+            var der = new RestSharp.Deserializers.JsonDeserializer();
+            var client = new RestClient(Fixtures.ADDRESS);
+
+            var response = client.Execute(request);
+
+            response.ResponseStatus.Should().Be(ResponseStatus.Completed);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            der.Deserialize<string>(response).Should().Be("Hello Tim");
+        }
+
+        [NUnit.Framework.Test]
+        public void EchoReturnsEchoedStringWithPOST()
+        {
+            var request = new RestRequest("api/test", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new LHAL.WebAPI.Models.TestEchoModel { Message = "Tim" });
+            var der = new RestSharp.Deserializers.JsonDeserializer();
+            var client = new RestClient(Fixtures.ADDRESS);
+
+            var response = client.Execute(request);
+
+            response.ResponseStatus.Should().Be(ResponseStatus.Completed);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            der.Deserialize<string>(response).Should().Be("Hello Tim");
+        }
+    }
+}
