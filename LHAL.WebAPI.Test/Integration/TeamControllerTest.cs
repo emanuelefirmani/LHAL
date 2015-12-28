2f8b13a9f2ed3a989bc07e60106c880832e8f763
Emanuele Firmani <emanuele.firmani@outlook.com>
2015-12-28 23:42:31 +0100

    More tests for TeamController

--- LHAL.WebAPI.Test/Integration/TeamControllerTest.cs
+++ LHAL.WebAPI.Test/Integration/TeamControllerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using FluentAssertions;
 using NUnit.Framework;
 using RestSharp;
@@ -16,6 +17,18 @@ namespace LHAL.WebAPI.Test.Integration
             var response = Fixtures.Client.Execute<Models.Team>(request);
 
             response.Data.Should().BeNull();
+        }
+
+        [TestCase("abc")]
+        [TestCase("")]
+        public void GETShouldReturnErrorIfTeamIDIsntValid(string teamID)
+        {
+            var request = new RestRequest("api/team/{teamID}", Method.GET);
+            request.AddUrlSegment("teamID", teamID);
+
+            var response = Fixtures.Client.Execute<Models.Team>(request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
 
         [Test]
