a67e44514f50d2007ee35526d20a45131409aac2
Emanuele Firmani <emanuele.firmani@outlook.com>
2016-01-08 23:28:06 +0100

    Service exposes API for retrieving player

---
+++ LHAL.WebAPI.Test/Integration/PlayerControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using FluentAssertions;
+using LHAL.WebAPI.Models;
+using NUnit.Framework;
+using RestSharp;
+
+namespace LHAL.WebAPI.Test.Integration
+{
+    class PlayerControllerTest
+    {
+        [Test]
+        public void APIPlayer_ShouldReturnNullIfPlayerIDDoesntExist()
+        {
+            var request = new RestRequest("v1/player/1000", Method.GET);
+
+            var response = Fixtures.Client.Execute<Player>(request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Data.Should().BeNull();
+        }
+
+        [TestCase("abc")]
+        [TestCase("")]
+        public void APIPlayer_ShouldReturnErrorIfPlayerIDIsntValid(string teamID)
+        {
+            var request = new RestRequest("v1/player/{playerID}", Method.GET);
+            request.AddUrlSegment("playerID", teamID);
+
+            var response = Fixtures.Client.Execute<Player>(request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ LHAL.WebAPI/Controllers/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using LHAL.WebAPI.Models;
+
+namespace LHAL.WebAPI.Controllers
+{
+    public class PlayerController : ApiController
+    {
+        [Route("v1/player/{playerID:int}")]
+        [HttpGet]
+        public Player Get(int playerID)
+        {
+            return null;
+        }
+    }
+}
